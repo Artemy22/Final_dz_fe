e0ce8cca0c8a13db382029b1c76a04d20bbae55f
Art <artem.nii@gmail.com>
2023-03-11 16:43:52 +0300

    sign-in tests done

--- cypress/e2e/sign-in.cy.js
+++ cypress/e2e/sign-in.cy.js
@@ -7,8 +7,28 @@ import user from '../fixtures/user.json'
 const loginPage = new LoginPage()
 
 describe('template spec', () => {
-  it('passes', () => {
+  it('Happy flow', () => {
     goToLoginPageBasic()
     loginPage.submitLoginForm(user)
+  })
+
+  it('Wrong email flow', () => {
+    goToLoginPageBasic()
+    loginPage.signInWrongEmail(user)
+  })
+
+  it('Empty email', () => {
+    goToLoginPageBasic()
+    loginPage.signInEmptyEmail(user)
+  })
+
+  it('Empty password', () => {
+    goToLoginPageBasic()
+    loginPage.signInEmptyPassword(user)
+  })
+
+  it('Wrong password flow', () => {
+    goToLoginPageBasic()
+    loginPage.signInWrongPassword(user)
   })
 })
--- cypress/support/pages/LoginPage.js
+++ cypress/support/pages/LoginPage.js
@@ -13,7 +13,6 @@ export default class LoginPage {
         return cy.get('[title="Login"]');
     }
 
-
     getContinueSignUpButton() {
         return cy.get('[title="Continue"]');
     }
@@ -48,6 +47,10 @@ export default class LoginPage {
 
     getErrorProvidePassword() {
         return cy.get('#mat-error-1')
+    }
+
+    getErrorProvideEmail() {
+        return cy.get('#mat-error-0')
     }
 
     getErrorInvalidEmailOrPassword() {
@@ -85,5 +88,35 @@ export default class LoginPage {
         this.loginButtonEnabled()
         this.getLoginButton().click()
         this.loggedInTrue()
+    }
+
+    signInWrongEmail(user) {
+        this.getEmailInput().type("qa@")
+        this.getPasswordInput().type(user.password);
+        this.getRememberMeCheckBox().click()
+        this.getLoginButton().click()
+        this.getErrorInvalidEmailOrPassword().should('exist')
+    }
+
+    signInWrongPassword(user) {
+        this.getEmailInput().type(user.email)
+        this.getPasswordInput().type("qaw");
+        this.getRememberMeCheckBox().click()
+        this.getLoginButton().click()
+        this.getErrorInvalidEmailOrPassword().should('exist')
+    }
+
+    signInEmptyEmail(user) {
+        this.getEmailInput().click()
+        this.getPasswordInput().click()
+        this.loginButtonDisabled()
+        this.getErrorProvideEmail().should('exist')
+    }
+
+    signInEmptyPassword(user) {
+        this.getPasswordInput().click()
+        this.loginButtonDisabled()
+        this.getEmailInput().click()
+        this.getErrorProvidePassword().should('exist')
     }
 }
